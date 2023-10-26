a4c1aa86220476753e436ab44291a7e6655b159e
buraxta <burakgulluler@gmail.com>
2023-10-26 20:43:36 +0300

    add remaining consumers

---
+++ src/AuctionService/Consumers/AuctionFinishedConsumer.cs
@@ -0,0 +1,34 @@
+﻿using AuctionService.Data;
+using Contarcts;
+using MassTransit;
+
+namespace AuctionService;
+
+public class AuctionFinishedConsumer : IConsumer<AuctionFinished>
+{
+    private readonly AuctionDbContext _context;
+
+    public AuctionFinishedConsumer(AuctionDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task Consume(ConsumeContext<AuctionFinished> context)
+    {
+        Console.WriteLine("--> Consuming AuctionFinished");
+
+        var auction = await _context.Auctions.FindAsync(context.Message.AuctionId);
+
+        if (context.Message.ItemSold)
+        {
+            auction.Winner = context.Message.Winner;
+            auction.SoldAmount = context.Message.Amount;
+        }
+
+        auction.Status = auction.SoldAmount > auction.ReservePrice ? Status.Finished : Status.ReserveNotMet;
+
+        await _context.SaveChangesAsync();
+    }
+
+
+}
